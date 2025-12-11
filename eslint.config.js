dffdc217a34da2aa331d9f48c05e5c1298a08556
Jeff Hanke <geophree@gmail.com>
2025-12-11 03:42:38 -0800

    Pull eslint ignore from .gitignore .

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import prettier from 'eslint-plugin-prettier/recommended';
 import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default [
-  {
-    ignores: ['.wrangler/**'],
-  },
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   {
     languageOptions: {
       globals: {
